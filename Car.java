9072691d660e298ebb8e0fe37949feed715c57bf
serhiylev <slevitsku@gmail.com>
2018-08-10 10:27:09 +0300

    added new method and changed names of the variables

--- Car.java
+++ Car.java
@@ -1,23 +1,31 @@
 public class Car {
     private String color;
-    private String brend;//марка
+    private String brand;//марка
     private double fuelDissipation; // розхіда на 1 км
     private double amountOfFuelInTheTank;//обʼєм пального в баці
     private double carMileage;//пробіг
     private double spentFuel;
 
+    Car(String color, String brend, int fuelDissipation, int amountOfFuelInTheTank, int carMileage) {
+        this.color = color;
+        this.brand = brend;
+        this.fuelDissipation = (fuelDissipation*0.01);
+        this.carMileage = carMileage;
+        this.amountOfFuelInTheTank = amountOfFuelInTheTank;
+    }
+
     void drive(double km) {
         spentFuel = fuelDissipation * km;
         if (spentFuel < amountOfFuelInTheTank) {
             carMileage += km;
-            System.out.println(color + " " + brend + " проїхав" + String.format("%.2f", km) + "км  загальний пробіг " + String.format("%.2f", carMileage));
+            System.out.println(color + " " + brand + " проїхав" + String.format("%.2f", km) + "км  загальний пробіг " + String.format("%.2f", carMileage));
             amountOfFuelInTheTank = amountOfFuelInTheTank - spentFuel;
         } else if (amountOfFuelInTheTank == 0) {
             System.out.println("Пального немає, заправте машину");
         } else {
             km = amountOfFuelInTheTank / fuelDissipation;
             carMileage += km;
-            System.out.println(color + " " + brend + " проїхав " + String.format("%.2f", km) + "км  загальний пробіг " + String.format("%.2f", carMileage));
+            System.out.println(color + " " + brand + " проїхав " + String.format("%.2f", km) + "км  загальний пробіг " + String.format("%.2f", carMileage));
             amountOfFuelInTheTank = 0;
         }
     }
@@ -26,19 +34,25 @@ public class Car {
         amountOfFuelInTheTank += fuelVolume;
     }
 
+    @Override
+    public String toString() {
+        return "Car{" +
+                "color='" + color + '\'' +
+                ", brand='" + brand + '\'' +
+                ", fuelDissipation=" + fuelDissipation +
+                ", amountOfFuelInTheTank=" + amountOfFuelInTheTank +
+                ", carMileage=" + carMileage +
+                ", spentFuel=" + spentFuel +
+                '}';
+    }
+
     void distance() {
         System.out.println("колір - " + color +
-                "\nмарка - " + brend +
-                "\nрозхід - " + fuelDissipation*100 +
+                "\nмарка - " + brand +
+                "\nрозхід - " + fuelDissipation * 100 +
                 "\nоб'єм пального в баці - " + amountOfFuelInTheTank +
                 "\nпробіг - " + String.format("%.2f", carMileage));
     }
 
-    Car(String color, String brend, int fuelDissipation, int amountOfFuelInTheTank, int carMileage) {
-        this.color = color;
-        this.brend = brend;
-        this.fuelDissipation = (fuelDissipation*0.01);
-        this.carMileage = carMileage;
-        this.amountOfFuelInTheTank = amountOfFuelInTheTank;
-    }
+
 }
